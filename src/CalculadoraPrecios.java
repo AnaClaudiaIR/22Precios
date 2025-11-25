280cff825ef14d5d7c1a86915d534d5b2cc7594e
AnaClaudiaIR <anac.ihnrod@educa.jcyl.es>
2025-11-25 11:46:20 +0100

    subir2

--- src/CalculadoraPrecios.java
+++ src/CalculadoraPrecios.java
@@ -1,5 +1,42 @@
-public class MainCalculadoraPrecios {
+import java.util.Scanner;
+
+public class CalculadoraPrecios {
     public static void main(String[] args) {
-        
+        Scanner scanner = new  Scanner(System.in);
+
+        System.out.println("Ingresa el precio base: ");
+        double precioBase = scanner.nextDouble();
+
+        double precioIVA = precioBase * 1.21;
+        System.out.println("Precio Base: " + precioBase);
+        System.out.printf("Precio IVA: %.2f\n", precioIVA);
+        System.out.println("----------------");
+        int menu;
+
+            System.out.println("Tipo de cliente: ");
+            System.out.println("1. Estudiante");
+            System.out.println("2. Adulto");
+            System.out.println("3. VIP");
+
+            menu = scanner.nextInt();
+
+            switch (menu) {
+                case 1:
+                    double precioEstudiante = precioIVA * ConstanteDescuento.estudiante; //Al ser estático se debe poner la clase a la que pertenece --> Es igual para todos
+                    System.out.printf("Precio Estudiante: %.2f", precioEstudiante);
+                    break;
+                case 2:
+                    double precioAdulto = precioIVA * ConstanteDescuento.adulto;
+                    System.out.printf("Precio Adulto: %.2f", precioAdulto);
+                    break;
+                case 3:
+                    double precioVip = precioIVA * ConstanteDescuento.VIP;
+                    System.out.printf("Precio VIP: %.2f", precioVip);
+                    break;
+
+                default:
+                    System.out.println("Opción no valida.");
+                    break;
+            }
     }
 }
--- src/ConstanteDescuento.java
+++ src/ConstanteDescuento.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class ConstanteDescuento {
+    //DESCUENTOS
+    public static final double estudiante = 0.8;
+    public static final double adulto = 0.7;
+    public static final double VIP = 0.85;
+
+    //IMPUESTO
+    public static final double IVA = 1.21;
 }
